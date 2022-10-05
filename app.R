035b4b3113e25256f1ba4f70f816014a7d0829fe
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-10-05 17:44:40 +0100

    various commmits

--- app.R
+++ app.R
@@ -1,50 +1,64 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(DT)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Editable Dataframe and Plot"),
+  
+  # Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      DTOutput("my_datatable"),
+      actionButton("go",label = "Plot Data")
+    ),
+    
+    # Show plot
+    mainPanel(
+      plotOutput("my_plot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  #initialize a blank dataframe
+  v <- reactiveValues(data = { 
+    data.frame(x = numeric(0),y = numeric(0)) %>% 
+      add_row(x = rep(0,10),y = rep(0,10))
+  })
+  
+  #output the datatable based on the dataframe (and make it editable)
+  output$my_datatable <- renderDT({
+    DT::datatable(v$data, editable = TRUE)
+  })
+  
+  #when there is any edit to a cell, write that edit to the initial dataframe
+  #check to make sure it's positive, if not convert
+  observeEvent(input$my_datatable_cell_edit, {
+    #get values
+    info = input$my_datatable_cell_edit
+    i = as.numeric(info$row)
+    j = as.numeric(info$col)
+    k = as.numeric(info$value)
+    if(k < 0){ #convert to positive if negative
+      k <- k * -1
+    }
+    
+    #write values to reactive
+    v$data[i,j] <- k
+  })
+  
+  #render plot
+  output$my_plot <- renderPlot({
+    req(input$go) #require the input button to be non-0 (ie: don't load the plot when the app first loads)
+    isolate(v$data) %>%  #don't react to any changes in the data
+      ggplot(aes(x,y)) +
+      geom_point() +
+      geom_smooth(method = "lm")
+  })
+  
 }
 
 # Run the application 
---
+++ app_old.R
@@ -0,0 +1,36 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyTime)
+ui <- fluidPage(
+  textInput("weight", "Weight"),
+  radioButtons("sex", "Sex", c("F", "M")),
+  timeInput("time", "Time 1:", value = Sys.time(), seconds = FALSE),
+  timeInput("time2", "Time 2:", value = Sys.time(), seconds = FALSE),
+  timeInput("time3", "Time 3:", value = Sys.time(), seconds = FALSE),
+  timeInput("time4", "Time 4:", value = Sys.time(), seconds = FALSE),
+  tableOutput("table")
+)
+
+server <- function(input, output, session) {
+  # Create a reactive expression
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+
+  
+  output$table <- renderTable({
+    dataset()
+  })
+}
+
+
+shinyApp(ui, server)
