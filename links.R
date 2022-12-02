804d2e54a614b188ad843fc886019b51a8105c44
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-12-02 10:05:42 +0000

    mastering shiny script added

--- links.R
+++ links.R
@@ -1,3 +1,5 @@
 http://www.stencilled.me/post/2019-04-18-editable/
   
   https://community.rstudio.com/t/how-do-i-create-an-editable-table-to-allow-user-input-draw-scatterplot-and-fit-a-curve-through-those-points/83802
+
+https://mastering-shiny.org/preface.html
